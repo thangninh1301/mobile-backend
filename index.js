7394c5c990ff94a94d01a43dca3e5df186588100
thang13199 <thang13199@gmail.com>
2020-05-21 12:50:48 +0700

    Sdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./src/app.express");
 const server = require("http").createServer(app);
 const config = require("config");
 
-const port = config.get("PORT");
+const port = process.env.PORT || config.get("PORT") ;
 
 server.listen(port, () => {
     console.log("Server is running on port: ", port);
