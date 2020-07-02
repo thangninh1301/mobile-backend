df595e9f99c536e606bd9b8f747b8aa47da8de29
Thang Ninh <thang@Thangs-MBP.lan>
2020-07-02 19:33:20 +0700

    df

--- src/controllers/accounts.controllers.js
+++ src/controllers/accounts.controllers.js
@@ -24,6 +24,16 @@ async function register(req, res) {
 
         await account.createUser(username,hashPassword);
 
+        const [existedShop] = await shopModels.getShopByName(name);
+        if (existedShop.length)
+            throw new Error("name is existed");
+
+        const [existedShop2] = await shopModels.getShopByUserId(id);
+        if (existedShop2.length)
+            throw new Error("user can create only 1 shop");
+
+        await shopModels.createShop(name,id);
+
         res.json(responseUtil.success({data: {}}));
     } catch (err) {
         res.json(responseUtil.fail({reason: err.message}));
